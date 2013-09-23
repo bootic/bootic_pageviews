1e4f1ffba7ec84c08932630ff7ffdae535626e18
Ismael Celis <ismaelct@gmail.com>
2013-09-23 16:47:38 +0100

    gofmt -w=true .

--- udp/udp.go
+++ udp/udp.go
@@ -1,37 +1,41 @@
 package udp
 
 import (
-  "net"
-  "log"
-  "encoding/json"
+	"encoding/json"
+	"log"
+	"net"
 )
 
 var udpConn *net.UDPConn
 
 type Publisher struct {
-  conn *net.UDPConn
+	conn *net.UDPConn
 }
 
-func NewPublisher(hostAndPort string) (*Publisher) {
-  udpAddr, err := net.ResolveUDPAddr("udp", hostAndPort)
-  if err != nil { panic("Could not connect to UDP server") }
-  var err2 error
-  udpConn, err2 = net.DialUDP("udp", nil, udpAddr) 
-  if err2 != nil { panic(err2) }
-  
-  pub := &Publisher{
-    conn: udpConn,
-  }
-  
-  return pub
+func NewPublisher(hostAndPort string) *Publisher {
+	udpAddr, err := net.ResolveUDPAddr("udp", hostAndPort)
+	if err != nil {
+		panic("Could not connect to UDP server")
+	}
+	var err2 error
+	udpConn, err2 = net.DialUDP("udp", nil, udpAddr)
+	if err2 != nil {
+		panic(err2)
+	}
+
+	pub := &Publisher{
+		conn: udpConn,
+	}
+
+	return pub
 }
 
 func (p *Publisher) Publish(event map[string]interface{}) {
-  json, err := json.Marshal(event)
-  
-  if err != nil {
-    log.Println("Could not marshal JSON:", err)
-  }
-  
-  p.conn.Write(json)
+	json, err := json.Marshal(event)
+
+	if err != nil {
+		log.Println("Could not marshal JSON:", err)
+	}
+
+	p.conn.Write(json)
 }
